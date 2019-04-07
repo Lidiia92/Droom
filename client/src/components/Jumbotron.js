fb0d417be0b5c17e5bde0d47f639d61040eb40b0
Lidii <lidiiany92@gmail.com>
2019-04-07 17:10:24 -0400

    Started styling jumbotron

--- client/src/components/Jumbotron.js
+++ client/src/components/Jumbotron.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import './styles/Jumbotron.css';
 
 const Jumbotron = () => (
     <div className="jumbotron__wrapper">
-
+        <h1 className="jumbotron__header">Welcome, to Droom</h1>
+        <p className="jumbotron__subheader">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porta consectetur nisl. Sed at vehicula justo.</p>
+        <p className="cta"><Link className="cta__button">Get Started!</Link></p>
+        <div className="cards">
+            <div className="cards__wrapper">
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+            </div>
+        </div>
     </div>
 );
 
