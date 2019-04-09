b3b441b9d9ccb0aadb22f07e51eabe5c691d65ec
wcolts2000 <wcolts2000@gmail.com>
2019-04-09 12:46:39 -0700

    added morgan & helmet to project and removed bod-parser as express has that bundled in now

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express = require("express");
+const express = require("express"),
+  cors = require("cors"),
+  morgan = require("morgan"),
+  helmet = require("helmet");
 
 const app = express();
+
+app.use(express.json(), cors(), morgan("dev"), helmet());
 
 app.get("/", (req, res) => {
   res.send("API is alive!!!");
