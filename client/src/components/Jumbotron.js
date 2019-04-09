fbbb62f75633ae59786307a9e996a7e289c3ff66
Lidii <lidiiany92@gmail.com>
2019-04-09 11:27:42 -0400

    Changed the colors

--- client/src/components/Jumbotron.js
+++ client/src/components/Jumbotron.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import './styles/Jumbotron.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faSearch} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUser, faSearch)
+
 const Jumbotron = () => (
     <div className="jumbotron__wrapper">
         <h1 className="jumbotron__header">Welcome, to Droom</h1>
@@ -9,8 +15,8 @@ const Jumbotron = () => (
         <p className="cta"><Link className="cta__button">Get Started!</Link></p>
         <div className="cards">
             <div className="cards__wrapper">
-                <div className="card"></div>
-                <div className="card"></div>
+                <div className="card"><FontAwesomeIcon className="card-svg" icon="user"/></div>
+                <div className="card"><FontAwesomeIcon className="card-svg" icon="search"/></div>
                 <div className="card"></div>
             </div>
         </div>
