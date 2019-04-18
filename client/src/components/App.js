8363bc797ddeb38659a72b84aedc937e576a0d2a
Lidii <lidiiany92@gmail.com>
2019-04-18 16:01:18 -0400

    Changed more styles on home page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,18 +12,17 @@ class App extends Component {
     return (
         <div className="App">
 			<NavBar />
-			<Banner />
-
 			<div className="banner__content-sm">
 				<div className="banner__text-sm">
-					<h1>GREAT CAREER AHEAD</h1>
-					<h3>Looking for a dream job?</h3>
-					<h3>You came to the right place</h3>
-					<p className="p-sm">Thousands job opportunities are within one click</p>
+					<div>
+						<h1>Finding your job is our job.</h1>
+					</div>
 					<button className="cta">GET STARTED!</button>
             		<AnchorLink className="cta cta-green" href='#about'>HOW DOES IT WORK?</AnchorLink>
 				</div>
 			</div>
+			<Banner />
+
 
 
 			<Jumbotron />
