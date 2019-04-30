6320ec782b9014bebfdaccef7715e5d23c7aca1f
Lidii <lidiiany92@gmail.com>
2019-04-29 20:35:57 -0400

    Updating array of  educations

--- client/src/components/Experience.js
+++ client/src/components/Experience.js
@@ -7,11 +7,29 @@ import './styles/PersonalInfo.css';
 
 
 
-
 const Experience = (props) => {
 
-    
-    
+    const [counter, setCounter] = useState(0)
+    const [educationArray, setEducationArray] = useState([0]);
+
+    function addEducation(newCounter) {
+
+        setCounter(newCounter + 1);
+        console.log(counter);
+        setEducationArray([...educationArray, newCounter + 1]);
+        console.log(educationArray);
+    }
+
+    function removeEducation(e, educationVal) {
+        e.preventDefault();
+        //setCounter(newCounter - 1);
+        console.log('value', educationVal);
+        setEducationArray(educationArray.filter((item) => item !== educationVal))
+        
+    }
+
+
+    console.log('test2', counter, educationArray);
     return (
 
         
@@ -40,32 +58,41 @@ const Experience = (props) => {
                 </div>
 
                 <div className="form__wrapper">
+
+                    {educationArray.map((education, index) => {
+                        return (
+                            <form key={education} className={index !== 0 ? `slide-fade slide-fade-show` : ''}>
+                                <div className="input__row">
+                                    <input className="input-sm" placeholder="School Name" />
+                                    <input className="input-sm" placeholder="Degree" />
+                                </div>
+
+                                <div className="input__row">
+                                    <input className="input-lg" placeholder="Field of Study" />
+                                </div>
+
+                                <div className="input__row">
+                                        <div className="labeled__input">
+                                            <label htmlFor="startDate" className="label">From Year</label>                 
+                                            <input id="startDate"  placeholder="Year" />  
+                                        </div>
+                                        <div className="labeled__input">
+                                            <label htmlFor="startDate" className="label">To Year</label>
+                                            <input id="startDate"  placeholder="Year" />                   
+                                        </div>
+                                </div> 
+
+                                <button className="button" type="submit">Save</button>
+                                {index !== 0 ? <button className="button btn-red" onClick={(e) => removeEducation(e, education)}>Cancel</button> : null}  
+
+                            </form>
+                        );
+                    })}
                 
-                    <form >
-                        <div className="input__row">
-                            <input className="input-sm" placeholder="School Name" />
-                            <input className="input-sm" placeholder="Degree" />
-                        </div>
 
-                        <div className="input__row">
-                            <input className="input-lg" placeholder="Field of Study" />
-                        </div>
-
-                        <div className="input__row">
-                                <div className="labeled__input">
-                                    <label htmlFor="startDate" className="label">From Year</label>                 
-                                    <input id="startDate"  placeholder="Field of Study" />  
-                                </div>
-                                <div className="labeled__input">
-                                    <label htmlFor="startDate" className="label">To Year</label>
-                                    <input id="startDate"  placeholder="Field of Study" />                   
-                                </div>
-                        </div>   
-
-                        <div className="input__row">
-                        </div>  
-
-                    </form>
+                    <div className="input__row">
+                        {educationArray.length === 4 ? null : <p className="add-more" onClick={ () => addEducation(counter)}>+ Add New Education</p>}
+                    </div>  
 
                 </div>
 
