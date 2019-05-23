3988fa5469a585ad6879afb2fbd2b9e434e2edfb
Lidii <lidiiany92@gmail.com>
2019-05-23 14:06:26 -0400

    more changes

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ if(process.env.NODE_ENV === "production") {
     //app.use(express.static('client/build'));
     app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
         
     });
