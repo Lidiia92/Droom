c8b9e34182994e07fcbd2ad06e2e26d1221b8022
Lidii <lidiiany92@gmail.com>
2019-05-23 15:55:09 -0400

    uncommented if statememnt

--- server.js
+++ server.js
@@ -47,15 +47,15 @@ mongoose
     .then(() => console.log('DB connected'))
     .catch(err => console.error(err));
 
-// if(process.env.NODE_ENV === "production") {
-//     app.use(express.static('client/build'));
-//     //app.use(express.static(path.join(__dirname, 'client', 'build')));
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'));
+    //app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
         
-//     });
-// }
+    });
+}
 
 const PORT = process.env.PORT || 3333;
 
