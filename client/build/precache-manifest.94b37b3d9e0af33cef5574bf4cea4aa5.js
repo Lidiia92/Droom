840dad57b90c1bab8a928cbd5b2bb48fc96d489f
Lidii <lidiiany92@gmail.com>
2019-05-23 14:20:11 -0400

    added build folder

---
+++ client/build/precache-manifest.94b37b3d9e0af33cef5574bf4cea4aa5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "64117e97a24abe1974cd861f8729fb95",
+    "url": "/static/media/608.64117e97.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c9d9d01d1eeacc9e69f3",
+    "url": "/static/js/main.0f5ddae5.chunk.js"
+  },
+  {
+    "revision": "99f17508244470371957",
+    "url": "/static/js/2.f1940c41.chunk.js"
+  },
+  {
+    "revision": "c9d9d01d1eeacc9e69f3",
+    "url": "/static/css/main.327d645a.chunk.css"
+  },
+  {
+    "revision": "99f17508244470371957",
+    "url": "/static/css/2.bc648c84.chunk.css"
+  },
+  {
+    "revision": "f3e0080c91e0e87d7a346ddbfac1e070",
+    "url": "/index.html"
+  }
+];
